3079312c6b192add662e2854da2ebc2e8c334be3
DevAlciri <alcirisoares74@gmail.com>
2024-08-26 09:33:51 -0300

    desafios aulao1 python

---
+++ Desafio01.py
@@ -0,0 +1,18 @@
+#Desafio01: Criar um script Python que leia o nome de uma pessoa e mostre uma  mensagem de boas-vindas de acordo com valor digitado.
+
+#nome = input ('Qual é o seu nome:')
+#print ('Seja muito bem vindo',nome)
+
+#Desafio02: Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+#dia = input('Digite o dia que você nasceu:')
+#mes = input('Digite o mês em que você nasceu:')
+#ano = input('Digite o ano que você nasceu:')
+#print ('você nasceu no dia',dia,'de',mes,'de',ano,'. Correto.')
+
+#Desafio03: Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+
+#n1 = int(input('Digite um número: '))
+#n2 = int(input('Digite outro número: #'))
+#soma = n1 + n2
+#print('A soma desses dois números é: ',soma)
